50e963301a37d88115b5807219b59ba4c3410fff
lauromiottojr <junior.miotto36@gmail.com>
2022-12-12 08:34:28 -0300

    Creating Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Link, Outlet } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <h2>movies lib</h2>
+      <Outlet />
     </div>
   )
 }
