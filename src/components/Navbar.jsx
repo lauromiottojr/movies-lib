8c0f5e71d37a8c10cf13f62208913f5698a1a7ba
lauromiottojr <junior.miotto36@gmail.com>
2022-12-12 09:47:47 -0300

    Config CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom'
 
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
+import './Navbar.css'
+
 const Navbar = () => {
     return (
         <nav id="navbar">
